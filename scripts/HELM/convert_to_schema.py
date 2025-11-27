ff95568c1827b4256f4e3f69aee9bb03f2285fef
Damian Stachura <damian.stachura@evidenceprime.com>
2025-11-27 01:30:43 +0100

    Fixed scripts to parse any tabs and full data for all HELM leaderboards

--- scripts/HELM/convert_to_schema.py
+++ scripts/HELM/convert_to_schema.py
@@ -25,7 +25,7 @@ from eval_types import (
 def parse_args():
     parser = ArgumentParser()
 
-    parser.add_argument('--leaderboard_name', type=str, default='HELM_Capabilities', choices=['HELM_Capabilities', 'HELM_Lite', 'HELM_Classic'])
+    parser.add_argument('--leaderboard_name', type=str, default='HELM_Capabilities', choices=['HELM_Capabilities', 'HELM_Lite', 'HELM_Classic', 'HELM_Instruct', 'HELM_MMLU'])
     parser.add_argument('--source_data_url', type=str, default='https://storage.googleapis.com/crfm-helm-public/capabilities/benchmark_output/releases/v1.12.0/groups/core_scenarios.json')
 
     args = parser.parse_args()
@@ -37,6 +37,7 @@ def download_leaderboard(url):
     response.raise_for_status()
 
     return response.json()
+
 
 def save_to_file(unified_eval_log: EvaluationLog, output_filepath: str) -> bool:
     try:
@@ -50,6 +51,7 @@ def save_to_file(unified_eval_log: EvaluationLog, output_filepath: str) -> bool:
         print(f"Problem with saving unified eval log to file: {e}")
         raise e
     
+
 def extract_generation_config_from_run_specs(run_specs: List[str]) -> Dict[str, Any]:
     generation_config = defaultdict(list)
 
@@ -70,22 +72,27 @@ def extract_generation_config_from_run_specs(run_specs: List[str]) -> Dict[str, 
     
     return generation_config
 
-def extract_model_info(row: List) -> ModelInfo:
-    model_name = row[0].get('value')
+
+def extract_model_info(row: List, model_name: str) -> ModelInfo:
     run_spec_names = next(
         (r["run_spec_names"] for r in row if "run_spec_names" in r),
         None
     )
-    spec = run_spec_names[0]
-    args = spec.split(':', 1)[1].split(',')
 
-    model_details = next(
-        (arg.split('=', 1)[1] for arg in args if arg.startswith('model=')),
-        ''
-    )
-    
-    developer = model_details.split('_')[0]
-    model_id = model_details.replace('_', '/')
+    if not run_spec_names:
+        developer = 'unknown'
+        model_id = model_name.replace(' ', '-')
+    else:
+        spec = run_spec_names[0]
+        args = spec.split(':', 1)[1].split(',')
+
+        model_details = next(
+            (arg.split('=', 1)[1] for arg in args if arg.startswith('model=')),
+            ''
+        )
+        
+        developer = model_details.split('_')[0]
+        model_id = model_details.replace('_', '/')
 
     return ModelInfo(
         name=model_name,
@@ -93,87 +100,84 @@ def extract_model_info(row: List) -> ModelInfo:
         developer=developer,
         inference_platform='unknown'
     )
-    
-def extract_efficiency_stats(efficiency_row: List) -> Tuple[float, List[float]]:
-    mean_win_rate = efficiency_row[1].get('value')
-    eval_times_for_benchmarks = [
-        stats.get('value') for stats in efficiency_row
+
+
+def check_if_values_are_in_range(tab_rows, min_value_in_range=0, max_value_in_range=1):
+    ''''
+    Check if we can assign range [0, 1] for metric score.
+    '''
+    number_of_columns = len(tab_rows[0]) - 1
+    mins = [0] * number_of_columns
+    maxs = [0] * number_of_columns
+    for row in tab_rows:
+        for col_id, el in enumerate(row):
+            if not col_id:
+                continue
+
+            mins[col_id - 1] = min(mins[col_id - 1], el.get('value', 0))
+            maxs[col_id - 1] = max(maxs[col_id - 1], el.get('value', 0))
+
+    return [
+        min_value >= min_value_in_range and max_value <= max_value_in_range
+        for min_value, max_value in zip(mins, maxs)
     ]
-    return mean_win_rate, eval_times_for_benchmarks
 
-def prepare_score_details(
-    acc_stats: Dict[str, Any], 
-    eff_stats: Dict[str, Any], 
-    column_idx: int,
-    leaderboard_name: str
-) -> ScoreDetails:
-    details = {
-        'accuracy_description': acc_stats.get('description'),
-        'efficiency_description': eff_stats.get('description')
-    }
-
-    if column_idx == 0 and leaderboard_name == 'helm_capabilities': # mean_score stats
-        details['mean_eval_time'] = round(eff_stats.get('value'), 3)
-    elif column_idx == 0 and leaderboard_name == 'helm_lite': # mean_win_rate stats
-        details['eval_time_mean_win_rate'] = round(eff_stats.get('value'), 3)
-    else:
-        details['eval_time'] = round(eff_stats.get('value'), 3)
-    
-    return ScoreDetails(
-        score=round(acc_stats.get('value'), 3),
-        details=details
-    )
 
 def convert(leaderboard_name, leaderboard_data, evaluation_source, source_data):
     '''
     Script for conversion data from leaderboards: HELM Capabilities, HELM Lite.
     '''
-    accuracy_tab_data = leaderboard_data[0]
-    accuracy_rows = accuracy_tab_data.get('rows')
-    headers = accuracy_tab_data.get('header')
-    eval_names = [header.get('value') for header in headers[1:]]
+    retrieved_timestamp = str(time.time())
+    model_to_results = defaultdict(list)
+    model_infos = {}
 
-    efficiency_rows = leaderboard_data[1].get('rows') if len(leaderboard_data) > 1 and leaderboard_data[1].get('title') == 'Efficiency' else []
+    tabs_headers = [tab.get('header') for tab in leaderboard_data]
+    tabs_rows = [tab.get('rows') for tab in leaderboard_data]
 
-    metrics = [
-        MetricConfig(
-            evaluation_description=header.get('description') or None,
-            lower_is_better=header.get('lower_is_better') or False,
-            min_score=0,
-            max_score=1,
-            score_type=ScoreType.continuous
-        )
-        for header in headers[1:]
-    ]
+    for idx, (headers, tab_rows) in enumerate(zip(tabs_headers, tabs_rows)):
+        tab_name = leaderboard_data[idx].get('title')
+        is_metric_in_range = check_if_values_are_in_range(tab_rows)
 
-    evaluation_logs = {}
+        for rows in tab_rows:
+            model_name = rows[0].get('value')
+            if model_name not in model_infos.keys():
+                model_infos[model_name] = extract_model_info(rows, model_name)
 
-    for acc_row, eff_row in list(zip(accuracy_rows, efficiency_rows)):
-        model_info = extract_model_info(acc_row)
-        retrieved_timestamp = str(time.time())
-        evaluation_id=f'{leaderboard_name}/{model_info.id.replace('/', '_')}/{retrieved_timestamp}'
-
-        evaluation_results = []
-        for column_idx, (acc_per_column, eff_per_column) in enumerate(zip(acc_row[1:], eff_row[1:])):
-            if not acc_per_column.get('value'):
-                continue
-
-            generation_config = extract_generation_config_from_run_specs(acc_per_column.get('run_spec_names')) if acc_per_column.get('run_spec_names') else {}
-
-            evaluation_results.append(
-                EvaluationResult(
-                    evaluation_name=eval_names[column_idx - 1],
-                    metric_config=metrics[column_idx - 1],
-                    score_details=prepare_score_details(
-                        acc_per_column,
-                        eff_per_column,
-                        column_idx,
-                        leaderboard_name
-                    ),
-                    generation_config=generation_config
+            for col_id, (header, row) in enumerate(zip(headers[1:], rows[1:])):
+                eval_name = header.get('value')
+                metric_config = MetricConfig(
+                    evaluation_description=header.get('description') or None,
+                    lower_is_better=header.get('lower_is_better') or False,
+                    min_score=0 if is_metric_in_range[col_id] else None,
+                    max_score=1 if is_metric_in_range[col_id] else None,
+                    score_type=ScoreType.continuous
                 )
-            )
 
+                score = row.get('value')
+
+                generation_config = extract_generation_config_from_run_specs(
+                    row.get('run_spec_names')
+                ) if row.get('run_spec_names') else {}
+
+                model_to_results[model_name].append(
+                    EvaluationResult(
+                        evaluation_name=eval_name,
+                        metric_config=metric_config,
+                        score_details=ScoreDetails(
+                            score=round(score, 3) if score else -1,
+                            details={
+                                "description": row.get('description', None),
+                                "tab": tab_name
+                            }
+                        ),
+                        generation_config=generation_config
+                    )
+                )
+
+
+    for model_name, evaluation_results in model_to_results.items():
+        model_info = model_infos.get(model_name)
+        evaluation_id=f'{leaderboard_name}/{model_info.id.replace('/', '_')}/{retrieved_timestamp}'
         eval_log = EvaluationLog(
             schema_version='0.0.1',
             evaluation_id=evaluation_id,
@@ -188,14 +192,15 @@ def convert(leaderboard_name, leaderboard_data, evaluation_source, source_data):
             evaluation_results=evaluation_results
         )
 
-        evaluation_logs[evaluation_id] = eval_log
-
         log_filename = f'{uuid.uuid4()}.json'
-        model_dev, model_name = tuple(model_info.id.split('/'))
-        dirpath = f'data/{leaderboard_name}/{model_dev}/{model_name}'
+        if model_info.developer == 'unknown':
+            model_name = model_info.id
+            dirpath = f'data/{leaderboard_name}/{model_name}'
+        else:
+            model_dev, model_name = tuple(model_info.id.split('/'))
+            dirpath = f'data/{leaderboard_name}/{model_dev}/{model_name}'
 
         os.makedirs(dirpath, exist_ok=True)
-
         save_to_file(eval_log, f'{dirpath}/{log_filename}')
 
 
@@ -215,4 +220,9 @@ if __name__ == '__main__':
     )
 
     convert(leaderboard_name, leaderboard_data, evaluation_source, source_data)
-    # 'https://storage.googleapis.com/crfm-helm-public/lite/benchmark_output/releases/v1.13.0/groups/core_scenarios.json'
+    # uv run python3 -m scripts.HELM.convert_to_schema --leaderboard_name HELM_Instruct --source_data_url https://storage.googleapis.com/crfm-helm-public/instruct/benchmark_output/releases/v1.0.0/groups/instruction_following.json
+    # https://storage.googleapis.com/crfm-helm-public/capabilities/benchmark_output/releases/v1.12.0/groups/core_scenarios.json
+    # https://storage.googleapis.com/crfm-helm-public/lite/benchmark_output/releases/v1.13.0/groups/core_scenarios.json
+    # https://storage.googleapis.com/crfm-helm-public/benchmark_output/releases/v0.4.0/groups/core_scenarios.json
+    # https://storage.googleapis.com/crfm-helm-public/instruct/benchmark_output/releases/v1.0.0/groups/instruction_following.json
+    # https://storage.googleapis.com/crfm-helm-public/mmlu/benchmark_output/releases/v1.13.0/groups/mmlu_subjects.json
